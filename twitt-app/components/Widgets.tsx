b48a922f6ccfea829ff35450a66e88f09ece59fe
Youssef Mhenni <youssef.mhenni@esprit.tn>
2022-06-16 14:37:50 +0100

    Add component widget

---
+++ twitt-app/components/Widgets.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SearchIcon} from "@heroicons/react/outline";
+import {TwitterTimelineEmbed} from "react-twitter-embed";
+
+function Widgets() {
+    return (
+        <div className= " mt-2 px-2">
+            {/* Search*/}
+            <div className=" mt-2 flex items-center
+             space-x-2 bg-gray-200 rounded-full p-3"
+             >
+                <SearchIcon className="h-5 w-5 text-gray-400"/>
+                <input className={"flex-1 bg-transparent outline-none"} type={"text"} placeholder={"search Twitter"}/>
+            </div>
+            <TwitterTimelineEmbed
+                sourceType="profile"
+                screenName="HAMZAUSS"
+                options={{height: 1000}}
+            />
+        </div>
+    );
+}
+
+export default Widgets;
--- twitt-app/pages/index.tsx
+++ twitt-app/pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
+import Widgets from "../components/Widgets";
 
 const Home: NextPage = () => {
   return (
@@ -14,7 +16,9 @@ const Home: NextPage = () => {
             {/*slider*/}
             <Sidebar/>
             {/*Feed*/}
+            <Feed/>
             {/*Widgets*/}
+            <Widgets/>
         </main>
 
     </div>
