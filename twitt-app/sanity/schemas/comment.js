44c315c58cc1ab77d67fdebaba8361b01b3e776a
Youssef Mhenni <youssef.mhenni@esprit.tn>
2022-06-17 11:08:53 +0100

    seetting local env and sanity shema setting '

--- twitt-app/sanity/schemas/comment.js
+++ twitt-app/sanity/schemas/comment.js
@@ -18,6 +18,15 @@ export default {
       title: 'Profile Image',
       type: 'string',
     },
+    {
+      name: 'tweet',
+      title: 'Tweet',
+      description:'Reference the Tweet the comment is associated to : ',
+      type: 'reference',
+      to:{
+        type:'tweet'
+      }
+    },
   ],
 
 }
