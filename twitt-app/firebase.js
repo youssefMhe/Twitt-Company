f425f8703dbb61deb487e647419fa68000e3b0f3
Youssef Mhenni <youssef.mhenni@esprit.tn>
2022-06-18 12:28:45 +0100

    Add Next-Auth and Firebase

---
+++ twitt-app/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBeCkpG1LA7zDqdW0yMtbOa2CY_42RxvPc",
+    authDomain: "twitt-app-61752.firebaseapp.com",
+    projectId: "twitt-app-61752",
+    storageBucket: "twitt-app-61752.appspot.com",
+    messagingSenderId: "204209723800",
+    appId: "1:204209723800:web:43421da224b19e745b33ed",
+    measurementId: "G-0T8B96NYDK"
+};
